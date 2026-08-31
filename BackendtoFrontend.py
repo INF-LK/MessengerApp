02e0418686dabc3ab827a33e01a0248ba6ede459
Dennis <dennispink5108@gmail.com>
2026-08-31 10:50:14 +0000

    Füge BackendtoFrontend-Klasse mit grundlegenden Importen und Kommentaren hinzu

---
+++ BackendtoFrontend.py
@@ -0,0 +1,8 @@
+import socket
+#wir brauchen:
+#wir kriegen eine Nachricht+Sender+Endpoint+public key(wir machen kein RSA!)
+#wir senden die nachricht an die Richtige person/port (->Von Datenbank)
+class BackendtoFrontend:
+    def __init__(self):
+        pass
+    def 
