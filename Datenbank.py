12ffb182aa832e1f6cefef3a8c06535855c32e1a
pitster21 <pitster21.zw@gmail.com>
2026-09-04 07:10:31 +0000

    neue datenbank weil die alte explodiert ist

--- Datenbank.py
+++ Datenbank.py
@@ -1,4 +1,5 @@
 #Hard at work or hardly working
+# or hard while working :)
 
 # format zum speichern: (Nutzer, Nachricht)
 
@@ -14,6 +15,11 @@ def setup_db():
          nutzer TEXT NOT NULL,
          nachricht TEXT NOT NULL)'''
     )
+        cursor.execute(
+           '''CREATE TABLE IF NOT EXISTS nutzer
+           (id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL UNIQUE)'''
+        )
     conn.commit()
     conn.close()
 
@@ -38,10 +44,33 @@ def auslesen_nachrichten():
     return daten
 
 
+def speichern_nutzer(name):
+    conn = sqlite3.connect("datenbank.db")
+    cursor = conn.cursor()
+    cursor.execute("INSERT OR IGNORE INTO nutzer (name) VALUES (?)", (name,))
+    conn.commit()
+    conn.close()
+
+
+def auslesen_nutzer():
+    conn = sqlite3.connect("datenbank.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM nutzer ORDER BY name")
+    daten = cursor.fetchall()
+    conn.close()
+    return daten
+
+
 def datenbank_reset():
     conn = sqlite3.connect("datenbank.db")
     cursor = conn.cursor()
     cursor.execute("DROP TABLE IF EXISTS nachrichten")
+    cursor.execute("DROP TABLE IF EXISTS nutzer")
     conn.commit()
     conn.close()
     setup_db()
+
+
+setup_db()
+speichern_nutzer("Max")
+print(auslesen_nutzer())
