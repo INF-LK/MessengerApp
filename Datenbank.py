c77010ad5abaac31aaf48b929a79872adf79c73e
pitster21 <pitster21.zw@gmail.com>
2026-09-04 06:52:37 +0000

    socket kommunikation

--- Datenbank.py
+++ Datenbank.py
@@ -1,10 +1,47 @@
 #Hard at work or hardly working
 
+# format zum speichern: (Nutzer, Nachricht)
 
 import sqlite3
 
-conn = sqlite3.connect("datenbank.db")
-cursor = conn.cursor()
 
-cursor_execute=()
+def setup_db():
+    conn = sqlite3.connect("datenbank.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS nachrichten
+        (id INTEGER PRIMARY KEY AUTOINCREMENT,
+         nutzer TEXT NOT NULL,
+         nachricht TEXT NOT NULL)'''
+    )
+    conn.commit()
+    conn.close()
 
+
+def speichern_nachricht(nutzer, nachricht):
+    conn = sqlite3.connect("datenbank.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO nachrichten (nutzer, nachricht) VALUES (?, ?)",
+        (nutzer, nachricht)
+    )
+    conn.commit()
+    conn.close()
+
+
+def auslesen_nachrichten():
+    conn = sqlite3.connect("datenbank.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM nachrichten")
+    daten = cursor.fetchall()
+    conn.close()
+    return daten
+
+
+def datenbank_reset():
+    conn = sqlite3.connect("datenbank.db")
+    cursor = conn.cursor()
+    cursor.execute("DROP TABLE IF EXISTS nachrichten")
+    conn.commit()
+    conn.close()
+    setup_db()
