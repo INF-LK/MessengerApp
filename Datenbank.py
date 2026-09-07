ac2b352dc5d9c79fcee0287f6e16f7061f127866
Felix Schweigmann <84675375+EinEisbxr@users.noreply.github.com>
2026-09-07 11:55:20 +0200

    Backend datenbank (#24)
    * Füge Datenbankverbindung und Cursor-Initialisierung hinzu
    
    * alles geht suppi -leo
    
    * socket kommunikation
    
    * neue datenbank weil die alte explodiert ist
    
    * fixed
    
    * datenbank-erweitert
    
    * neue-datenbank-erweitert
    
    * Delete datenbank.db
    
    ---------
    
    Co-authored-by: pitster21 <pitster21.zw@gmail.com>

---
+++ Datenbank.py
@@ -0,0 +1,83 @@
+#Hard at work or hardly working
+# or hard while working :)
+
+# format zum speichern: (Nutzer, Nachricht)
+
+import sqlite3
+
+
+def setup_db():
+    conn = sqlite3.connect("datenbank.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS nachrichten
+        (id INTEGER PRIMARY KEY AUTOINCREMENT,
+         nutzer TEXT NOT NULL,
+         nachricht TEXT NOT NULL)'''
+    )
+    cursor.execute(
+           '''CREATE TABLE IF NOT EXISTS nutzer
+           (id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL UNIQUE)'''
+        )
+    conn.commit()
+    conn.close()
+
+
+def speichern_nachricht(nutzer, nachricht):
+    conn = sqlite3.connect("datenbank.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO nachrichten (nutzer, nachricht) VALUES (?, ?)",
+        (nutzer, nachricht)
+    )
+    conn.commit()
+    conn.close()
+
+
+def auslesen_nachrichten():
+    conn = sqlite3.connect("datenbank.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM nachrichten")
+    daten = cursor.fetchall()
+    conn.close()
+    return daten
+
+
+def auslesen_nachrichten_von_nutzer(nutzer):
+    conn = sqlite3.connect("datenbank.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT * FROM nachrichten WHERE nutzer = ?",
+        (nutzer,)
+    )
+    daten = cursor.fetchall()
+    conn.close()
+    return daten
+
+
+def speichern_nutzer(name):
+    conn = sqlite3.connect("datenbank.db")
+    cursor = conn.cursor()
+    cursor.execute("INSERT OR IGNORE INTO nutzer (name) VALUES (?)", (name,))
+    conn.commit()
+    conn.close()
+
+
+def auslesen_nutzer():
+    conn = sqlite3.connect("datenbank.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM nutzer ORDER BY name")
+    daten = cursor.fetchall()
+    conn.close()
+    return daten
+
+
+def datenbank_reset():
+    conn = sqlite3.connect("datenbank.db")
+    cursor = conn.cursor()
+    cursor.execute("DROP TABLE IF EXISTS nachrichten")
+    cursor.execute("DROP TABLE IF EXISTS nutzer")
+    conn.commit()
+    conn.close()
+    setup_db()
